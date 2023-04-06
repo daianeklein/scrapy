17e9e07a1d1bc38ca0c244fa300f0967dad06b79
daianeklein <daiane.klein22@gmail.com>
2023-04-06 12:45:31 -0300

    add scrapy quotes

--- scrapy_Tutorial/quotes_to_scrape/quotes_to_scrape/spiders/quotes.py
+++ scrapy_Tutorial/quotes_to_scrape/quotes_to_scrape/spiders/quotes.py
@@ -7,9 +7,23 @@ class QuotesSpider(scrapy.Spider):
     start_urls = ["http://quotes.toscrape.com/"]
 
     def parse(self, response):
-        #author = response.css('author::text').extract()
-        author = response.xpath('//*[@class="author"]/text()').extract()
-        page_number = response.css('li.next a').xpath('@href').extract()
+        all_div_quotes = response.css('div.quote')
         
-        yield {'Author' : author,
-                'page_number' : page_number }
+        title = all_div_quotes.css('span.text::text').extract()
+        quotes = all_div_quotes.css('.author::text').extract()
+        tag = all_div_quotes.css('.tag::text').extract()
+
+        yield {'title' : title,
+                'quotes' : quotes,
+                'tag' : tag}
+
+
+
+
+
+        #author = response.css('author::text').extract()
+        # author = response.xpath('//*[@class="author"]/text()').extract()
+        # page_number = response.css('li.next a').xpath('@href').extract()
+        
+        # yield {'Author' : author,
+        #         'page_number' : page_number }
