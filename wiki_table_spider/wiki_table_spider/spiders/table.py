a95a6539ea9aaa07ce7d5f298314afffe1350cc0
daianeklein <daiane.klein22@gmail.com>
2023-04-11 11:56:45 -0300

    finish table scraper

--- wiki_table_spider/wiki_table_spider/spiders/table.py
+++ wiki_table_spider/wiki_table_spider/spiders/table.py
@@ -7,15 +7,16 @@ class TableSpider(scrapy.Spider):
     start_urls = ["https://en.wikipedia.org/wiki/List_of_U.S._states_and_territories_by_population"]
 
     def parse(self, response):
-        table = response.xpath('//table[contains(@class, "wikitable sortable")]')[0]
-        trs = table.xpath('//tr')[2:]
+        table = response.xpath('//table[contains(@class, "wikitable sortable")]')[1]
 
-        ranks = trs.xpath('//span[@data-sort-value]/text()').extract()
-        states = trs.xpath('//tr/th/a/text()').extract()
+        ranks = table.xpath('//span[@data-sort-value]/text()').extract()
+        states = table.xpath('//tr/th/a/text()').extract()
+        population = table.xpath('//tr/td[1]/following-sibling::td[3]/text()').extract()
         
-        for rank, state in zip(ranks, states):
+        for rank, state, population in zip(ranks, states, population):
             yield {'rank' : rank,
-                    'state' : state}
+                    'state' : state,
+                    'population' : population}
 
 
 
