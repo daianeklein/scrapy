19ab0959eec8e6529e1ac79b5c98666c951de58c
daianeklein <daiane.klein22@gmail.com>
2023-06-05 20:45:27 -0300

    finish flask project

--- splash/steam/web/app.py
+++ splash/steam/web/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
 import requests
-import json
 
 app = Flask(__name__)
 
@@ -10,18 +9,8 @@ def index():
         url = 'http://127.0.0.1:9080/crawl.json?start_requests=true&spider_name=best_selling'
     ).json()
 
-    items = json.dumps(resp.get('items'))
-    return items
-
-@app.route('/show')
-def show_template():
-    users_list = [
-        'User 1',
-        'User 2',
-        'User 3'
-    ]
-    return render_template('index.html', users_list = users_list)
-
+    items = resp.get('items')
+    return render_template('index.html', games = items)
 
 if __name__ == "__main__":
     app.run(port=8000, debug=True)
